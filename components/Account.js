5b2524ed8bd4598f5f70545f70ee740b3d59ac98
Wonyoung Choi <wc7qpe@virginia.edu>
2021-11-25 22:19:50 -0500

    added accounts page

--- components/Account.js
+++ components/Account.js
@@ -1,11 +1,38 @@
 // display name, statistics, logout button
 
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { Text, View, Button } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 
-export default function Account() {
+export default function Account({setLoginToken}) {
+
+    const [name, setName] = useState("");
+
+    useEffect(() => { // gets name of current user
+        SecureStore.getItemAsync('token').then((token) => {
+                fetch(`https://graph.facebook.com/me?access_token=${token}`).then((response) => {
+                        response.json().then((object) =>{
+                            setName(object.name);
+                        });
+                }).catch((err) => {
+                    console.log(err);
+                })
+        }).catch((err) => {
+            console.log(err);
+        });
+    }, [])
+
+    const logout = async () => {
+        setLoginToken(undefined);
+    }
+
     return (
-        <Text>Account</Text>
-    )
+        <View>
+            <Text>Logged in as {name} </Text>
+            <Button 
+                title="Logout" 
+                onPress={() => logout()}
+            />
+        </View>
+    );
 }
